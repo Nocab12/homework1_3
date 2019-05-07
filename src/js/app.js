47d164790976022f66159ef0fa64e40567dd8641
Denis Berezin <nocab@yandex.ru>
2019-05-07 20:48:06 +0300

    task3

---
+++ src/js/app.js
@@ -0,0 +1,21 @@
+/*one funciton*/
+function test() {
+    var sum = [300, 200, 100,600, 500, 800, 10000, 5, 700];
+    for (var i=0; i < sum.length; i++) {
+        if (a < sum[i]) {
+            a = sum[i];
+        }
+
+    }
+    console.log(a);
+}
+test();
+/*two function*/
+var numbers = [74989, 74990, 84990, 62000, 58480, 61800];
+numbers.sort(function(a, b) {
+    return a - b;
+});
+numbers.reverse();
+var gradePoint = (numbers[0] + numbers[1] + numbers[2]) / 3;
+/*console.log(numbers);*/ /*вывод упорядоченного массива*/
+console.log(gradePoint);
